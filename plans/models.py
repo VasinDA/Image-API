4b1fcde916911c54363f81e03fe8b78cbe66fae9
Denis Vasin <svga.shop@gmail.com>
2023-02-19 22:18:24 +0200

    api

--- plans/models.py
+++ plans/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 class Plan(models.Model):
     title = models.CharField(max_length=50)
-    image_file = models.FileField()
+    image_url = models.ImageField(upload_to='images/')
+    expires_after = models.IntegerField(default=300)
+
+    def generate_link_with_expiration(self):
+        expires_at = timezone.now() + timezone.timedelta(seconds=self.expires_after)
+        link = Plan.objects.create(image_url=self.image_url, expires_at=expires_at)
+        return link.image_url
+    
+    
+    
+
+    
+
+    
