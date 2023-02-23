00395847b85a1deddab0efdc22de2625844f3584
Denis Vasin <svga.shop@gmail.com>
2023-02-23 18:20:25 +0200

    venv

--- images/urls.py
+++ images/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import ListImage, DetailsImage, OriginalView
+from .views import ListImage, DetailsImage, OriginalView, BinaryView
 
 
 urlpatterns = [
     path('image', ListImage.as_view(), name='image_list'),
     path('image/<int:pk>/', DetailsImage.as_view(), name='image_details'),
     path('image/<int:pk>/original', OriginalView.as_view(), name='image_original'),
-    
+    path('image/<int:pk>/binary', BinaryView.as_view(), name='image_binary'),
 ]
