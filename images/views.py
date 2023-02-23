f8f9ce1cfc4c4bb811252b974ea0afdb4318f1c7
Denis Vasin <svga.shop@gmail.com>
2023-02-23 16:01:44 +0200

    binary view

--- images/views.py
+++ images/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Image
-from django.http import HttpResponse
+from django.shortcuts import  get_object_or_404
+from django.http import HttpResponse, FileResponse
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ImageSerializer
@@ -20,7 +21,18 @@ class DetailsImage(generics.RetrieveAPIView):
 class OriginalView(generics.RetrieveAPIView):
     serializer_class = ImageSerializer
     def get(self, request, pk):
-        image = Image.objects.get(pk=pk)
+        image =  get_object_or_404(Image, pk=pk)
         image_url = image.image.url[1:]
         with open(image_url, 'rb') as f:
             return HttpResponse(f.read(), content_type="image/jpeg")
+
+class BinaryView(generics.RetrieveAPIView):
+    serializer_class = ImageSerializer
+    def get(self, request, pk):
+        image = get_object_or_404(Image, pk)
+        image_url = image.image.url[1:]
+        with open(image_url, 'rb') as f:
+           response = HttpResponse(content = f)
+           response['Content-Type'] = 'image/jpeg'
+           response['Content-Disposition'] = 'attachment; filename="binary.jpg"'
+           return response
