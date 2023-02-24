72929f6eef91ff7ae21097da64a0e466b5c95530
Denis Vasin <svga.shop@gmail.com>
2023-02-24 23:51:37 +0200

    images urls

--- images/urls.py
+++ images/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('image/<int:pk>/original', OriginalView.as_view(), name='image_original'),
     path('image/<int:pk>/binary', BinaryView.as_view(), name='image_binary'),
     path('media/user_<pk>/<pk1>/<pk2>', LinkView.as_view(), name='image_link'),
-    path('image/<int:pk>/thumbnail/<pk1>', ThumbnailView.as_view, ame='thumbnail_link')
+    path('image/<int:pk>/thumbnail/<pk1>', ThumbnailView.as_view(), name='thumbnail_link')
     
     ]
