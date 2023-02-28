983ad4813e78db692d49cc964efa314c4f5a8951
Denis Vasin <svga.shop@gmail.com>
2023-02-28 02:04:14 +0200

    plans tests

--- plans/tests.py
+++ plans/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from .models import Plan
 
-# Create your tests here.
+class PlansTest(TestCase):
+    def setUp(self):
+        self.plan = Plan.objects.create(
+            title='Basic', 
+            original_image_link = False, 
+            binary_image_link = False, 
+            available_hights = '200')
+        
+    def test_plan_model(self):
+        self.assertEqual(self.plan.title, 'Basic')
+        self.assertEqual(self.plan.original_image_link, False)
+        self.assertEqual(self.plan.binary_image_link, False)
+        self.assertEqual(self.plan.available_hights, '200')
+
+    def test_validate_available_hights(self):
+        wrong_values_list = ['a', '!', ',', '@', '200,a', 'a,200']
+        for value in wrong_values_list:
+            self.assertRaisesMessage(Plan.objects.create(
+                title='Basic', 
+                original_image_link = False, 
+                binary_image_link = False, 
+                available_hights = value), "Enter numbers")
